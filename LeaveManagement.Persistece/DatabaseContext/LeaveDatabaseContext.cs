70ff313b7cb0b0e10e1b55ae737b307523c12ff7
f00000b <f00000b@gmail.com>
2023-07-18 11:08:06 +0330

    DbContextSetting and Implemention

---
+++ LeaveManagement.Persistece/DatabaseContext/LeaveDatabaseContext.cs
@@ -0,0 +1,63 @@
+﻿using LeaveManagement.Domain;
+using LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Persistence.DatabaseContext;
+
+public class LeaveDatabaseContext : DbContext
+{
+    public LeaveDatabaseContext(DbContextOptions<LeaveDatabaseContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<LeaveType> LeaveTypes { get; set; }
+    public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveDatabaseContext).Assembly);
+        
+        #region Comment
+        //modelBuilder.ApplyConfiguration(new LeaveTypeConfiguration());
+
+        //Moved to Configuration
+        //modelBuilder.Entity<LeaveType>().HasData(
+        //{
+        //    new LeaveType
+        //    {
+        //        Id = 1,
+        //        Name = "Vacation",
+        //        DefaultDays = 10,
+        //        DateCreated = DateTime.Now,
+        //        DateModified = DateTime.Now
+
+        //    };
+
+        //}; 
+        #endregion
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+            .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
+        {
+            entry.Entity.DateModified = DateTime.Now;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.DateCreated = DateTime.Now;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+}
+
+
+
---
+++ LeaveManagement.Persistece/PersistenceServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using LeaveManagement.Application.Contracts.Persistence;
+using LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeaveManagement.Persistence;
+
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveDatabaseContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("LeaveDatabaseConnectionString"));
+            });
+
+
+            return services;
+        }
+    }
+
