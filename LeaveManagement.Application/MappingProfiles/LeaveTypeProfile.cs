dd0bdc9e1942c4fbfbc23475c9c7b4190ee05bca
f00000b <f00000b@gmail.com>
2023-07-18 08:20:00 +0330

    Adding Autompper,MediatR Adding AutoMApperProfile Adding AppServiceRegistration

---
+++ LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveManagement.Domain;
+
+namespace LeaveManagement.Application.MappingProfiles;
+
+    public class LeaveTypeProfile : Profile
+    {
+        public LeaveTypeProfile()
+        {
+            CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDetailsDto>();
+        }
+    }
+
